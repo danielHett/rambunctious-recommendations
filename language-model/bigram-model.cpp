11ed8a3dff5508d7eaa39d1dabbf1747a3f08173
Daniel Hettinger <danielhettinger@wirelessprv-10-194-119-201.near.illinois.edu>
2022-06-23 11:15:30 -0500

    Implement GetCount

--- language-model/bigram-model.cpp
+++ language-model/bigram-model.cpp
@@ -26,3 +26,16 @@ void BigramModel::ReadText(string text_path) {
 
   return;
 }
+
+size_t BigramModel::GetCount(string w) {
+  // If the word w does not exist in our model, the count is equal to zero!
+  if (bigram_counts_.count(w) == 0) return 0;
+
+  // Otherwise, we get a map to all of the words that follow w, and their counts.
+  map<string, size_t> counts = bigram_counts_.at(w);
+  size_t total = 0;
+
+  for (std::map<string,size_t>::iterator it=counts.begin(); it!=counts.end(); ++it) total += it->second;
+
+  return total;
+}
